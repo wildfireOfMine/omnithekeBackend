b962d6a18dff7e689f167d9aa721117b2f3861f3
wildfireOfMine <iamtheanguera@gmail.com>
2026-04-18 17:48:59 +0200

    Fixed some DB issues

--- doctorApp/models.py
+++ doctorApp/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Doctor(models.Model):
     djangoUser = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    firstSurname = models.CharField(max_length=120, null=False, blank=False)
-    secondSurname = models.CharField(max_length=120, null=False, blank=False)
+    firstSurname = models.CharField(max_length=120, null=True, blank=True)
+    secondSurname = models.CharField(max_length=120, null=True, blank=True)
     email = models.EmailField(null=False, blank=False)
     birthdate = models.DateField()
     identityDocument = models.CharField(max_length=20, unique=True)
