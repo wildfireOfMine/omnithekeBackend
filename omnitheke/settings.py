307d777404705141efc681747d2ec061356574e4
wildfireOfMine <iamtheanguera@gmail.com>
2026-06-01 18:17:52 +0200

    Adapted Allowed Hosts for the FrontEnd

--- omnitheke/settings.py
+++ omnitheke/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['omnitheke.vercel.app']
 
 
 # Application definition
