a3534f4950f735ef503fbcee2f7548fb985ef7eb
wildfireOfMine <iamtheanguera@gmail.com>
2026-06-02 17:37:21 +0200

    Added views for calendar

--- patientApp/urls.py
+++ patientApp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("messages/", views.messagesView.as_view()),
     path("appointments/", views.appointmentsView.as_view()),
     path("appointments/<int:pk>/", views.appointmentsPKView.as_view()),
+    path("availability/<str:date>/<int:pk>/", views.availabilityView.as_view())
 ]
