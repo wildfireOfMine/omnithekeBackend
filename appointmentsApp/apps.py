b2d795703d27a44d928522a578436cd13071d138
wildfireOfMine <iamtheanguera@gmail.com>
2026-08-13 14:17:13 +0200

    Added more models in the database

---
+++ appointmentsApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppointmentsappConfig(AppConfig):
+    name = 'appointmentsApp'
--- usersApp/admin.py
+++ usersApp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from usersApp.models import *
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "rol",
+        "is_active",
+        "is_staff",
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Información adicional", {
+            "fields": ("rol",),
+        }),
+    )
+
+admin.site.register(Especialidad)
+admin.site.register(Doctor)
