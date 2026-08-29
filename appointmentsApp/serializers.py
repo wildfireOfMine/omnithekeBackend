c8f6bffcae3a5128a593d1290136c073abe8baee
wildfireOfMine <iamtheanguera@gmail.com>
2026-08-29 19:48:12 +0200

    Added a new field to Appointment serializer

--- appointmentsApp/serializers.py
+++ appointmentsApp/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from appointmentsApp.models import Cita
 
 class CitaSerializer(serializers.ModelSerializer):
+    doctor = serializers.SerializerMethodField()
     class Meta:
         model = Cita
         fields = "__all__"
+
+    def get_doctor(self, obj):
+        doctor = obj.calendario.doctor
+        return f"{doctor.nombre} {doctor.primerApellido}"
